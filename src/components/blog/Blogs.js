8ac97980a6386a3c846ee29aba0c7fb8fbfb8ed2
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-11-11 12:23:01 +0330

    writing query for getting data for blogs

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,6 +1,14 @@
+import { useQuery } from "@apollo/client";
 import React from "react";
+import GET_BLOGS_INFO from "../graghql/queries";
 
 function Blogs() {
+  const { loading, data, error } = useQuery(GET_BLOGS_INFO);
+
+  if (loading) return <h4>Loading ...</h4>;
+
+  if (error) return <h4>Error ...</h4>;
+  console.log({ loading, data, error });
   return <div>Blogs</div>;
 }
 
