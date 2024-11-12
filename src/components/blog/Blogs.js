95094603c33481d64a731d4afa4410b325631967
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-11-12 09:29:56 +0330

    design weblogs and added ability resposive to them

--- src/components/blog/Blogs.js
+++ src/components/blog/Blogs.js
@@ -1,6 +1,8 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import GET_BLOGS_INFO from "../graghql/queries";
+import { Grid2 } from "@mui/material";
+import CardEL from "../shared/CardEL";
 
 function Blogs() {
   const { loading, data, error } = useQuery(GET_BLOGS_INFO);
@@ -9,7 +11,15 @@ function Blogs() {
 
   if (error) return <h4>Error ...</h4>;
   console.log({ loading, data, error });
-  return <div>Blogs</div>;
+  return (
+    <Grid2 container spacing={2}>
+      {data.posts.map((post) => (
+        <Grid2 size={{ xs: 12, sm: 6, md: 4 }} key={post.id}>
+          <CardEL {...post} />
+        </Grid2>
+      ))}
+    </Grid2>
+  );
 }
 
 export default Blogs;
