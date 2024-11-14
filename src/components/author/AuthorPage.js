fd4e62613e8234bfbc6199803dea63c111de986a
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-11-14 09:47:08 +0330

    added datas authors

--- src/components/author/AuthorPage.js
+++ src/components/author/AuthorPage.js
@@ -47,6 +47,9 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { GET_AUTHOR_INFO } from "../graghql/queries";
 import { Avatar, Container, Grid2, Typography } from "@mui/material";
+import CardEL from "../shared/CardEL";
+
+import sanitizeHtml from "sanitize-html";
 
 function AuthorPage() {
   const { slug } = useParams();
@@ -59,7 +62,9 @@ function AuthorPage() {
   if (error) return <h4>error ...</h4>;
   console.log(data);
 
-  const { author } = data;
+  const {
+    author: { name, field, avatar, description, posts },
+  } = data;
 
   return (
     <div>
@@ -81,20 +86,40 @@ function AuthorPage() {
           sx={{ textAlign: "center" }}
         >
           <Grid2 item>
-            <Avatar src={author.avatar.url} sx={{ width: 250, height: 250 }} />
+            <Avatar src={avatar.url} sx={{ width: 250, height: 250 }} />
           </Grid2>
           <Grid2 item>
             <Typography component="h3" variant="h5" fontWeight={700} mt={4}>
-              {author.name}
+              {name}
             </Typography>
           </Grid2>
           <Grid2 item>
             <Typography component="p" variant="h5" color="text.secondary">
-              {author.field}
+              {field}
             </Typography>
           </Grid2>
-          <Grid2 item xs={12}>
-            {author.description.html}
+          <Grid2 item xs={12} mt={5}>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: sanitizeHtml(description.html),
+              }}
+            ></div>
+          </Grid2>
+          <Grid2 item xs={12} mt={6}>
+            <Typography component="h3" variant="h5" fontWeight={700}>
+              مقالات {name}
+            </Typography>
+            <Grid2 container spacing={2} mt={2}>
+              {posts.map((post) => (
+                <Grid2 item xs={12} sm={6} md={4} key={post.id}>
+                  <CardEL
+                    title={post.title}
+                    slug={post.slug}
+                    coverPhoto={post.coverPhoto}
+                  />
+                </Grid2>
+              ))}
+            </Grid2>
           </Grid2>
         </Grid2>
       </Container>
